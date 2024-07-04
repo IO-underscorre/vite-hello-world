f70c6c0dd47b662ab5c842dc362e63d43452e528
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-05 01:39:27 +0200

    feat: page completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'AppHero'
+}
+</script>
+
+<template>
+    <div class="jumbo">
+        <span>
+            Con un incredibile
+        </span>
+        <h2>
+            JUMBOTRON
+        </h2>
+    </div>
+</template>
+
+<style scoped>
+.jumbo {
+    height: 50vh;
+    padding: 5rem;
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+}
+
+h2 {
+    font-size: 4rem;
+    align-self: flex-end;
+    text-shadow: 0 0 15px var(--details-color);
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppTitle'
+}
+</script>
+
+<template>
+    <h1>
+        La mia prima app con <span>Vite</span>!
+    </h1>
+</template>
+
+<style scoped>
+span {
+    color: var(--details-color);
+}
+</style>
